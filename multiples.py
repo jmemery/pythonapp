599d401bf4e2bc90a56ced581186bf1c3cb85cc3
John Emery <jmemery@crimson.ua.edu>
2017-06-05 11:26:30 -0700

    multiples

---
+++ multiples.py
@@ -0,0 +1,16 @@
+#multiples
+print count in range(1,1001,2)
+    print count
+
+#print multiples of B
+for count in range(5,1000001,5)
+    print count
+#sum list
+a = [1,2,5,10,255,3]
+mysum = 0
+for i in a:
+    mysum+= i
+    print mysum
+
+#print avg
+print mysum/len(a)
