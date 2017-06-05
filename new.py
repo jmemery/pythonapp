5ecb8c82a95af28cc706c6afda824f2e31b35498
John Emery <jmemery@crimson.ua.edu>
2017-06-05 11:16:08 -0700

    added py

--- new.py
+++ new.py
@@ -1,1 +1,13 @@
-print "Hello world!"
+str = "The quick brown fox jumped over the lazy dog"
+print str
+print str.find("brown")
+str = str.replace("brown","red")
+print str
+#min and max
+x = [2,54,-2,7,12,98]
+print x
+print min(x)
+print max(x)
+
+x = ["hello",2,54,-2,7,12,98,"world"]
+print x[0], x[len(x)-1]
