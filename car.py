92219767bb5462128114130ff98bc6751e79da42
John Emery <jmemery@crimson.ua.edu>
2017-06-07 10:57:47 -0700

    python OOP

--- car.py
+++ car.py
@@ -1,2 +1,21 @@
-class Car(object):
-    
+class Car:
+    def __init__(self, price, speed, fuel, mileage, tax):
+        self.price = price
+        self.speed = speed
+        self.fuel = fuel
+        self.mileage = mileage
+        self.tax = tax
+    def display_all(self):
+        return '{} {} {} {} {}'.format(self.price, self.speed, self.fuel, self.mileage, self.tax)
+        return self
+emp_1 = Car('20000','35mph','full', '99mpg', '15%')
+emp_2 = Car('40000', '99mph', 'Kinda full', '7mpg', '15%')
+emp_3 = Car('2000', '30mph', 'empty', '190mpg', '12%')
+emp_4 = Car('10000', '40mph', 'full', '70mpg', '12%')
+emp_5 = Car('1000000','50mph', 'full', '6mpg', '15%')
+#print('{}{}'.format(emp_1.first, emp_1.last))
+print(emp_1.display_all())
+print(emp_2.display_all())
+print(emp_3.display_all())
+print(emp_4.display_all())
+print(emp_5.display_all())
--- grades.py
+++ grades.py
@@ -3,15 +3,15 @@ import random
 def grade(scores):
     print "Scores and Grades"
     for x in range(0, scores):
-        score = random.randint(60, 101)
+        score = random.randint(50, 100)
         if score >= 60 and score <= 69:
-            print "Score: ", score,"; Your grade is a D"
+            print "Score: ", score,", Your grade is a D"
         elif score >= 70 and score <= 79:
-            print "Score: ", score, "; Your grade is a C"
+            print "Score: ", score, ", Your grade is a C"
         elif score >= 80 and score <= 89:
-            print "Score: ", score, "; Your grade is a B"
+            print "Score: ", score, ", Your grade is a B"
         elif score >= 90 and score <= 100:
-            print "Score: ", score, "; Your grade is an A"
+            print "Score: ", score, ", Your grade is an A"
         else:
             print "You failed"
     print "End of the program.  Bye!"
